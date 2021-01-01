9e3c9e7954c7cf0536d923f84053911fb28b4d22
Christopher Lebron <christopher.lebron1@gmail.com>
2020-12-31 22:41:07 -0500

    Added Table. Still missing logic.

--- JS/site.js
+++ JS/site.js
@@ -2,30 +2,6 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
-function Mortgage(amount, rate, terms) {
-   this.amount = amount; 
-   this.rate = rate;
-   this.terms = terms;
-   this.balance = 0;
-   this.GetMonthlyPayment = function() {
-       this.rate = this.rate/100;
-       M = this.amount/12 * ((this.rate * Math.pow(1+this.rate,this.terms))/(Math.pow(1+this.rate,this.terms)-1));
-       return M.toFixed(2);        
-    }
-}
-
-    document.querySelector('form').onsubmit = function () {
-
-    let _amount = document.querySelector('#loanAmount').value;
-    let _rate = document.querySelector('#interestRate').value; 
-    let _terms = document.querySelector('#terms').value; 
-
-    var Loan = new Mortgage(_amount, _rate, _terms);
-    document.querySelector('#payment').innerHTML = `Montly Payment: $ ${Loan.GetMonthlyPayment()}`;
-    
-    return false;
-}
-
 if (hourNow > 18){
     greeting = 'Good Evening!';
 } else if (hourNow > 12){
@@ -37,3 +13,27 @@ if (hourNow > 18){
 }
 
 document.write('<h3 class = "text-center">' + greeting + '</h3>');
+
+function Mortgage(amount, rate, terms) {
+   this.amount = amount; 
+   this.rate = rate;
+   this.terms = terms;
+   this.balance = amount;
+   this.GetMonthlyPayment = function() {
+       this.rate = this.rate/100;
+       M = this.amount/12 * ((this.rate * Math.pow(1+this.rate,this.terms))/(Math.pow(1+this.rate,this.terms)-1));
+       return M.toFixed(2);        
+    }
+}
+
+    document.querySelector('form').onsubmit = function () {
+    let _amount = document.querySelector('#loanAmount').value;
+    let _rate = document.querySelector('#interestRate').value; 
+    let _terms = document.querySelector('#terms').value; 
+
+    var Loan = new Mortgage(_amount, _rate, _terms);
+
+    document.querySelector('#payment').innerHTML = `Montly Payment: $ ${Loan.GetMonthlyPayment()}`;
+    document.querySelector('#initBalance').innerHTML = `Initial Balance: $ ${Loan.balance}`;
+    return false;
+}
